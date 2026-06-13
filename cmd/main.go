a6f604ae396115f0b3d67d63aa521988c15da8de
iamhaghighi <iamhaghighi@gmail.com>
2026-06-13 21:53:14 +0330

    🎨style(version): add flag version

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v" || os.Args[1] == "version") {
+	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v" || os.Args[1] == "version" || os.Args[1] == "v") {
 		fmt.Println("Todo Cli Management " + constants.VERSION)
 		return
 	}
